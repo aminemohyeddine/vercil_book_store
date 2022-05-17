2b0f5362ed0bac1871e65817fe1883e8c8ffbe02
Amine Mohyeddine <amohyeddine@smb.sqli.com>
2022-05-17 17:40:12 +0100

    first

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,7 @@ import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    origin: [
-      /^(https?:\/\/([^\.]*\.)?main--storied-puppy-f7ef20.netlify\.app)$/i,
-      /^(https?:\/\/([^\.]*\.)?localhost:3000)$/i,
-    ],
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-    credentials: true,
-  });
+  const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   await app.listen(parseInt(process.env.PORT) || 5000);
 }
