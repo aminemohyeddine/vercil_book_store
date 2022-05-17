5eaad11f24a1425e74e81f9a315f378c079f4ed1
Amine Mohyeddine <amohyeddine@smb.sqli.com>
2022-05-17 17:20:36 +0100

    first

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
     origin: [
-      /^(https?:\/\/([^\.]*\.)?6283c6f24a6f022e6d37a3a3--storied-puppy-f7ef20.netlify\.app)$/i,
-      /^(https?:\/\/([^\.]*\.)?localhost:3000\.com)$/i,
+      /^(https?:\/\/([^\.]*\.)?storied-puppy-f7ef20.netlify\.app)$/i,
+      /^(https?:\/\/([^\.]*\.)?localhost:3000)$/i,
     ],
     allowedHeaders:
       'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
