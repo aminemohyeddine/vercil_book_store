e5640ba09fa76763b33bcf9e9e24f03b710ea5e7
Amine Mohyeddine <amohyeddine@smb.sqli.com>
2022-05-17 16:25:55 +0100

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,21 @@ import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
+  var whitelist = [
+    'https://sweet-eclair-6fba13.netlify.app',
+    'http://localhost:3000',
+  ];
+
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
   app.use(cookieParser());
   await app.listen(parseInt(process.env.PORT) || 5000);
 }
